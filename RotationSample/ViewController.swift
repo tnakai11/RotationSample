6c01e40ab902ac55491b8d38e6dc6a17e131950a
tnakai11 <tnakai@m.cs.osakafu-u.ac.jp>
2021-02-09 16:15:55 +0900

    Initial Commit

---
+++ RotationSample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RotationSample
+//
+//  Created by Takumi Nakai on 2021/02/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
